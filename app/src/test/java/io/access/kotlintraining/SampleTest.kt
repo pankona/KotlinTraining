e5b66eee5f3c762471b541f86c6a6850c4470d48
pankona <yosuke.akatsuka@access-company.com>
2018-04-18 23:22:22 +0900

    introduce kotlin.test as a dependency. update test code

--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -1,11 +1,17 @@
 package io.access.kotlintraining
 
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class SampleTest {
     @Test
     fun testWelcome() {
         welcome()
+
+        // Example of test
+        val expected = 1
+        val actual = 1
+        assertEquals(expected, actual, "[want] $expected [got] $actual")
     }
 
     @Test
