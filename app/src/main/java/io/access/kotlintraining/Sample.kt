78bede6992b5089ddf103959fd718d312a77a73c
pankona <yosuke.akatsuka@access-company.com>
2018-04-18 21:48:03 +0900

    add first examples

---
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -0,0 +1,7 @@
+package io.access.kotlintraining
+
+fun welcome() {
+    println("welcome to ACCESS!")
+}
+
+// TODO: declare hello function
---
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -0,0 +1,15 @@
+package io.access.kotlintraining
+
+import org.junit.Test
+
+class SampleTest {
+    @Test
+    fun testWelcome() {
+        welcome()
+    }
+
+    @Test
+    fun testHello() {
+        // TODO: implement to call hello function
+    }
+}
