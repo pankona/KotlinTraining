03bfbb57293752079ae93e0bd21e577210850605
kazuma yabuki <kazuma.yabuki@access-company.com>
2020-04-02 19:16:58 +0900

    Exercise (1) Fizz Buzz Unit test

--- app/src/main/java/io/access/kotlintraining/Sample.kt
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -15,13 +15,22 @@ fun main(args: Array<String>) {
 }
 
 fun fizzBuzz() {
-    for (i in 1..100) {
+        for (i in 1..100) {
 
-        when {
-            i % 15 == 0 -> println("FizzBuzz")
-            i % 5 == 0 -> println("Buzz")
-            i % 3 == 0 -> println("Fizz")
-            else -> println("$i")
+            when {
+                i % 15 == 0 -> println("FizzBuzz")
+                i % 5 == 0 -> println("Buzz")
+                i % 3 == 0 -> println("Fizz")
+                else -> println("$i")
+            }
         }
+}
+
+fun fizzBuzzForTest(i: Int): String {
+    return when {
+        i % 15 == 0 -> "FizzBuzz"
+        i % 5 == 0 -> "Buzz"
+        i % 3 == 0 -> "Fizz"
+        else -> "$i"
     }
 }
--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -19,3 +19,17 @@ class SampleTest {
         // TODO: implement to call hello function
     }
 }
+
+class FizzBuzzTest {
+    @Test
+    fun testFizzBuzz() {
+        // TODO: テストを書く
+        // assertEquals 等の assert 系 function を使おう
+        // 以下の kotlin.test パッケージのリファレンスも参照のこと
+        // https://kotlinlang.org/api/latest/kotlin.test/kotlin.test/index.html
+        assertEquals("FizzBuzz", fizzBuzzForTest(15))
+        assertEquals("8", fizzBuzzForTest(8))
+        assertEquals("Fizz", fizzBuzzForTest(12))
+        assertEquals("Buzz", fizzBuzzForTest(35))
+    }
+}
