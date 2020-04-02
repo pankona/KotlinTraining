d4440e2ba0332762a23c311bc54fb8598de01f60
kazuma yabuki <kazuma.yabuki@access-company.com>
2020-04-02 19:07:00 +0900

    Exercise (1) Fizz Buzzの解答

--- app/src/main/java/io/access/kotlintraining/Sample.kt
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -6,5 +6,22 @@ fun welcome() {
 
 // TODO: declare hello function
 fun main(args: Array<String>) {
+    //Hello World!
     println("Hello world")
+
+
+    //Fizz Buzz
+    fizzBuzz()
+}
+
+fun fizzBuzz() {
+    for (i in 1..100) {
+
+        when {
+            i % 15 == 0 -> println("FizzBuzz")
+            i % 5 == 0 -> println("Buzz")
+            i % 3 == 0 -> println("Fizz")
+            else -> println("$i")
+        }
+    }
 }
